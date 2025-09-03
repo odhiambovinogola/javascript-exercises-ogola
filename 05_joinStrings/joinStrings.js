7b9e2df3cbf0b99bfaa35a4a4005e656ce97a774
odhiambovinogola <141261098+odhiambovinogola@users.noreply.github.com>
2025-09-03 15:27:26 +0300

    Fix fullName spacing and enable joinStrings tests

--- 05_joinStrings/joinStrings.js
+++ 05_joinStrings/joinStrings.js
@@ -13,7 +13,7 @@ let lastName = "Stevenson";
 let thisYear = 1965;
 let birthYear = 1947;
 
-let fullName = firstName + "  " + lastName;
+let fullName = firstName + " " + lastName;
 let age = thisYear - birthYear;
 
 let greeting = `Hello! My name is ${fullName} and I am ${age} years old.`;
