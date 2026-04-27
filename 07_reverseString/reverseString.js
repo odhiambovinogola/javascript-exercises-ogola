d103ad2defb3ccd07afa2414e8686d690ed487a4
Odhiambo Vincent Ogola <141261098+odhiambovinogola@users.noreply.github.com>
2026-04-27 20:51:30 +0300

    feat(reverseString): implement string reversal with type validation

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+// Reverses a string. Throws TypeError if input is not a string.
+const reverseString = function (str) {
+  if (typeof str !== "string") {
+    throw new TypeError("Expected a string, but received " + typeof str);
+  }
+  return str.split("").toReversed().join("");
 };
 
 // Do not edit below this line
