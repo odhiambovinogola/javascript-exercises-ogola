c5b8ca8ba8a893528e54a9d0ab27e9ceb8b7bb96
Odhiambo Vincent Ogola <141261098+odhiambovinogola@users.noreply.github.com>
2026-05-02 19:38:55 +0300

    feat(tempConversion): implement Celsius and Fahrenheit conversion with rounding

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  let result = (fahrenheit - 32) / 1.8;
+  return Math.round(result * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  let result = celsius * 1.8 + 32;
+  return Math.round(result * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
